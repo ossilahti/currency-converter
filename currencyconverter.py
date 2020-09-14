adc29245301246977fc02f2749890222db9ffd95
ossilahti <ossi.lahti5@gmail.com>
2020-09-14 16:52:08 +0300

    Finished project.

--- currencyconverter.py
+++ currencyconverter.py
@@ -7,8 +7,10 @@ from forex_python.converter import CurrencyRates
 def converter(from_curr, to_curr):
     convert = c.get_rate(from_curr, to_curr)
     input_field = int(entry.get()) * convert
-    label = tk.Label(root, text=input_field)
-    label.grid(row=3, columnspan=3)
+    label = tk.Label(root, text=input_field, font=("Georgia", 15))
+    label.grid(row=4, column=1)
+    label2 = tk.Label(root, text=to_curr, font=("Georgia", 15))
+    label2.grid(row=4, column=2)
 
 
 # Is a callback function for the button 'Convert'.
@@ -29,12 +31,16 @@ def swap():
 # Initializes the window
 c = CurrencyRates()
 root = tk.Tk()
-root.geometry('250x300')
+root.geometry('300x300')
 root.title("Currency Converter")
 
+# A label to the top of the window
+banner_label = tk.Label(root, text="Currency Converter 1.0", font=("Georgia", 15), fg="green", padx=10, pady=10)
+banner_label.grid(row=0, columnspan=3)
+
 # Entry/text box
-entry = tk.Entry(root, width=33, borderwidth=3)
-entry.grid(columnspan=3, padx=10, pady=10)
+entry = tk.Entry(root, width=13)
+entry.grid(row=3, column=1, padx=10, pady=10)
 
 # Makes the combobox for from currencies
 from_currency = tk.StringVar()
@@ -43,7 +49,7 @@ from_currencies = ttk.Combobox(root, width=10, textvariable=from_currency, state
 # Adding combobox drop down list
 from_currencies['values'] = ('EUR', 'USD', 'GBP')
 from_currencies.grid(column=1, row=1)
-from_curr_label = tk.Label(root, text="From: ")
+from_curr_label = tk.Label(root, text="From: ", padx=10, font=("Arial", 10))
 from_curr_label.grid(row=1, column=0)
 
 # Shows EUROs as a default value
@@ -56,18 +62,18 @@ to_currencies = ttk.Combobox(root, width=10, textvariable=to_currency, state="re
 # Adding combobox drop down list
 to_currencies['values'] = ('EUR', 'USD', 'GBP')
 to_currencies.grid(column=1, row=2)
-to_curr_label = tk.Label(root, text="To: ")
+to_curr_label = tk.Label(root, text="To: ", padx=10, font=("Arial", 10))
 to_curr_label.grid(row=2, column=0)
 
 # Shows DOLLARs as a default value
 to_currencies.current(1)
 
 # Makes the 'Swap' -button
-swap = tk.Button(root, text="Swap", command=swap, width=7)
-swap.grid(row=1, column=2)
+swap = tk.Button(root, text="Swap", command=swap, width=7, font=("Arial", 9))
+swap.grid(row=2, column=2)
 
 # Makes the 'Convert' -button
-convert_button = tk.Button(root, text="Convert", command=callback, width=7)
-convert_button.grid(row=2, column=2)
+convert_button = tk.Button(root, text="Convert", command=callback, width=7, font=("Arial", 9))
+convert_button.grid(row=3, column=2)
 
 root.mainloop()
